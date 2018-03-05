a6aa0fda45bbcab96c6cea53517d197e9740c0de
mikecalex <mikecalex@gmail.com>
2018-03-05 12:26:36 -0500

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Exercise designed to go through the steps of implementing a multi-page Sinatra-and-React portfolio site using React Router. 
---
+++ react/src/components/BackButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { browserHistory } from 'react-router'
+
+const BackButton = () => {
+  return(
+    <div>
+      Back
+    </div>
+  )
+}
+
+export default BackButton;
